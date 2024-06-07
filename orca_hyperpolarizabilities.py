423c1f95ba822da90161b4520fff566f9f8547d0
Petru Milev <139863355+Petru-Milev@users.noreply.github.com>
2024-06-07 17:28:52 +0200

    orca_hyperpol 2

--- orca_hyperpolarizabilities.py
+++ orca_hyperpolarizabilities.py
@@ -1,8 +1,8 @@
 import numpy as np 
 import os
-from functions_for_library import romberg_procedure 
+from romberg import romberg_procedure
 #from romberg import romberg_procedure
-import girona_donostia as gd
+
 
 def create_inputs(path_input, path_output=None, derivative_type='romberg', romberg_points = 6 ):
 
@@ -82,6 +82,7 @@ def extract_polarizability_tensor(path_file):
     return alpha_tensor, diagonalized_tensor, P
         
 def extract_data_from_folder(path_folder):
+    
 
     def sort_data(data): 
         data_x = data[data[:, 0] != 0]
@@ -102,6 +103,7 @@ def extract_data_from_folder(path_folder):
         if not file.endswith('.out'):
             continue
         if ('efield' not in file) and ('_origin' in file):
+            print(file)
             alpha_tensor, diagonalized_tensor, P = extract_polarizability_tensor(os.path.join(path_folder, file))
             alpha_tensor = alpha_tensor.flatten()
             line = np.concatenate((np.zeros(3), alpha_tensor))
@@ -141,34 +143,195 @@ def calc_beta_from_alpha(data):
 
     #print(data_z[:, 2])
 
-    beta_xxx = -gd.romberg_procedure(data_x[:, 0], data_x[:, 3])
-    beta_xyy = -gd.romberg_procedure(data_y[:, 1], data_y[:, 4])
-    beta_xzz = -gd.romberg_procedure(data_z[:, 2], data_z[:, 5])
-    beta_yxx = -gd.romberg_procedure(data_x[:, 0], data_x[:, 6])
-    beta_yyy = -gd.romberg_procedure(data_y[:, 1], data_y[:, 7])
-    beta_yzz = -gd.romberg_procedure(data_z[:, 2], data_z[:, 8])
-    beta_zxx = -gd.romberg_procedure(data_x[:, 0], data_x[:, 9])
-    beta_zyy = -gd.romberg_procedure(data_y[:, 1], data_y[:, 10])
-    beta_zzz = -gd.romberg_procedure(data_z[:, 2], data_z[:, 11])
-    beta_xyz = -gd.romberg_procedure(data_z[:, 2], data_z[:, 4])
+    beta_xxx = romberg_procedure(data_x[:, 0], data_x[:, 3])
+    beta_xyy = romberg_procedure(data_y[:, 1], data_y[:, 4])
+    beta_xzz = romberg_procedure(data_z[:, 2], data_z[:, 5])
+    beta_yxx = romberg_procedure(data_x[:, 0], data_x[:, 6])
+    beta_yyy = romberg_procedure(data_y[:, 1], data_y[:, 7])
+    beta_yzz = romberg_procedure(data_z[:, 2], data_z[:, 8])
+    beta_zxx = romberg_procedure(data_x[:, 0], data_x[:, 9])
+    beta_zyy = romberg_procedure(data_y[:, 1], data_y[:, 10])
+    beta_zzz = romberg_procedure(data_z[:, 2], data_z[:, 11])
+    beta_xyz = romberg_procedure(data_z[:, 2], data_z[:, 4])
 
     beta = np.array([beta_xxx, beta_xyy, beta_xzz, beta_yxx, beta_yyy, beta_yzz, beta_zxx, beta_zyy, beta_zzz, beta_xyz])
     return beta
 
-
-
+def extract_beta_from_gaussian_output(path_file):
+    """
+    Extracting beta values from gaussian log file 
+    Extracting  Beta (dipole orientation)  Beta(0;0,0):
+    output is in the following format 
+    return np.array([beta_xxx, beta_xyy, beta_xzz, beta_yxx, beta_yyy, beta_yzz, beta_zxx, beta_zyy, beta_zzz, beta_xyz])
+    """
+    beta = None
+    with open(path_file, 'r') as f:
+        lines = f.readlines()
     
 
+    for i, line in enumerate(lines):
+        if ('Beta(0;0,0)' in line) and ('(dipole orientation)' in lines[i-4]):
+            lines = lines[i+8:i+18]
+            break
+    else:
+        print('Beta not found in the file')
+        return beta
+    
+    dict_beta_comp = {}
+    #Not iterating through beta values 
+    for i, line in enumerate(lines):
+        line = line.split()
+        dir = line[0]
+        val = float(line[1].replace('D', 'e')) #replacing 0.137447D+03
+        dict_beta_comp[dir] = val
+    beta = np.array([dict_beta_comp['xxx'], dict_beta_comp['yxy'], dict_beta_comp['zxz'], dict_beta_comp['xxy'], dict_beta_comp['yyy'], dict_beta_comp['zyz'], dict_beta_comp['xxz'], dict_beta_comp['yyz'], dict_beta_comp['zzz'], dict_beta_comp['yxz']])
+    return beta
 
+
+
+
+
+
+
+def get_beta_quantities(beta_vector):
+    def get_beta_3d(beta_vector):
+        beta_xyz = beta_vector[-1]
+        beta = beta_vector[:-1]
+        beta = beta.reshape((3,3))
+        """beta = 
+        [[beta_xxx, beta_xyy, beta_xzz],
+        [beta_yxx, beta_yyy, beta_yzz],
+        [beta_zxx, beta_zyy, beta_zzz]]"""
+        beta_3d = np.zeros((3,3,3))
+        beta_3d[:,:,:] = None
+        beta_3d[0,0,0] = beta[0,0]
+        beta_3d[0,1,1] = beta[0,1]
+        beta_3d[0,2,2] = beta[0,2]
+        beta_3d[1,0,0] = beta[1,0]
+        beta_3d[1,1,1] = beta[1,1]
+        beta_3d[1,2,2] = beta[1,2]
+        beta_3d[2,0,0] = beta[2,0]
+        beta_3d[2,1,1] = beta[2,1]
+        beta_3d[2,2,2] = beta[2,2]
+        beta_3d[0,1,2] = beta_xyz
+        
+        has_NaN = True
+        while has_NaN:
+            has_NaN = np.any(np.isnan(beta_3d))
+            for i in range(3):
+                for j in range(3):
+                    for k in range(3):
+                        if np.isnan(beta_3d[i,j,k]):
+                            beta_3d[i,j,k] = beta_3d[i,k,j]
+            for i in range(3):
+                for j in range(3):
+                    for k in range(3):
+                        if np.isnan(beta_3d[i,j,k]):
+                            beta_3d[i,j,k] = beta_3d[k,j,i]
+        return beta_3d
+    
+    def avg_beta_zzz_sqr(beta_3d):
+        if beta_3d.shape != (3,3,3):
+            raise ValueError('Beta tensor must be 3x3x3')
+        
+        indices = range(3)
+
+        term1 = (1/7) * sum(beta_3d[i,i,i]**2 for i in indices)
+        term2 = (6/35) * sum(beta_3d[i, i, i] * beta_3d[i,j,j] for i in indices for j in indices if i != j)
+        term3 = (9/35) * sum(beta_3d[i, j, j]**2 for i in indices for j in indices if i != j)
+        term4 = (9/105) * sum(beta_3d[i, j, j]*beta_3d[i, k, k] for i in indices for j in indices for k in indices if i != j and i != k and j != k )
+        term5 = (1/70) * sum(beta_3d[i, j, k]**2 for i in indices for j in indices for k in indices if i != j and i != k and j != k)
+
+        result = term1 + term2 + term3 + term4 + term5
+        return result
+
+    def avg_beta_zxx_sqr(beta_3d):
+        if beta_3d.shape != (3,3,3):
+            raise ValueError('Beta tensor must be 3x3x3')
+        
+        indices = range(3)
+
+        term1 = (1/35)*sum(beta_3d[i,i,i]**2 for i in indices)
+        term2 = (2/105)*sum(beta_3d[i,i,i] * beta_3d[i, j, j] for i in indices for j in indices if i != j)
+        term3 = (11/105)*sum(beta_3d[i, j, j]**2 for i in indices for j in indices if i != j)
+        term4 = (1/105)*sum(beta_3d[i, j, j] * beta_3d[i, k, k] for i in indices for j in indices for k in indices if i != j and i != k and j != j)
+        term5 = (1/105)* sum(beta_3d[i,j,k]**2 for i in indices for j in indices for k in indices if i != j and i != k and j != k)
+        result = term1 - term2 + term3 - term4 + term5
+        return result
+    
+    def DR(avg_beta_zzz_sqr, avg_beta_zxx_sqr):
+        return avg_beta_zzz_sqr / avg_beta_zxx_sqr
+    
+    def beta_hrs(avg_beta_zzz_sqr, avg_beta_zxx_sqr):
+        DR = avg_beta_zzz_sqr / avg_beta_zxx_sqr
+        beta_hrs = np.sqrt(avg_beta_zzz_sqr * (1 + 1/DR))
+        return beta_hrs
+    
+    def beta_j_one(beta_3d):
+        if beta_3d.shape != (3,3,3):
+            raise ValueError('Beta tensor must be 3x3x3')
+        
+        indices = range(3)
+
+        term1 = (2/5) * sum(beta_3d[i,i,i]**2 for i in indices)
+        term2 = (6/5) * sum(beta_3d[i, i, i] * beta_3d[i,j,j] for i in indices for j in indices if i != j)
+        term3 = (3/5) * sum(beta_3d[i, j, j]**2 for i in indices for j in indices if i != j)
+        term4 = (3/5) * sum(beta_3d[i, j, j]*beta_3d[i, k, k] for i in indices for j in indices for k in indices if i != j and i != k and j != k )
+
+        result = term1 + term2 + term3 + term4
+        return result
+    
+    def beta_j_three(beta_3d):
+        if beta_3d.shape != (3,3,3):
+            raise ValueError('Beta tensor must be 3x3x3')
+        
+        indices = range(3)
+
+        term1 = (2/5) * sum(beta_3d[i,i,i]**2 for i in indices)
+        term2 = (6/5) * sum(beta_3d[i, i, i] * beta_3d[i,j,j] for i in indices for j in indices if i != j)
+        term3 = (12/5) * sum(beta_3d[i, j, j]**2 for i in indices for j in indices if i != j)
+        term4 = (3/5) * sum(beta_3d[i, j, j]*beta_3d[i, k, k] for i in indices for j in indices for k in indices if i != j and i != k and j != k )
+        term5 = (1/4) * sum(beta_3d[i, j, k]**2 for i in indices for j in indices for k in indices if i != j and i != k and j != k)
+
+        result = term1 - term2 + term3 - term4 + term5 
+        return result   
+    
+    def anisotropy_parameter(beta_j_three, beta_j_one):
+        return beta_j_three/beta_j_one
+    
+    beta_3d = get_beta_3d(beta_vector)
+    print(f"Shape of beta3d is {beta_3d.shape}")
+    var_avg_beta_zzz_sqr = avg_beta_zzz_sqr(beta_3d)
+    var_avg_beta_zxx_sqr = avg_beta_zxx_sqr(beta_3d)
+    var_DR = DR(var_avg_beta_zzz_sqr, var_avg_beta_zxx_sqr)
+    var_beta_hrs = beta_hrs(var_avg_beta_zzz_sqr, var_avg_beta_zxx_sqr)
+    var_beta_j_one = beta_j_one(beta_3d)
+    var_beta_j_three = beta_j_three(beta_3d)
+    var_anisotropy_parameter = anisotropy_parameter(var_beta_j_three, var_beta_j_one)
+
+    lst = [var_avg_beta_zzz_sqr, var_avg_beta_zxx_sqr, var_DR, var_beta_hrs, var_beta_j_one, var_beta_j_three, var_anisotropy_parameter]
+    
+    return lst
 
 
 
 if __name__ == '__main__':
     path = "/Users/petrumilev/Library/CloudStorage/GoogleDrive-petia.md36@gmail.com/Other computers/Do_not_delete_from_here/a.me/Master CNE/Study Materials/Master_Thesis/Ruben_Derivatives/Geom1_LiH.inp"
-    folder_output = "/Users/petrumilev/Desktop/orca_outputs"
-    path_output = "/Users/petrumilev/Library/CloudStorage/GoogleDrive-petia.md36@gmail.com/Other computers/Do_not_delete_from_here/a.me/Master CNE/Study Materials/Master_Thesis/Ruben_Derivatives/E_Y/a1_Y_-0.0004.out"
+    #folder_output = "/Users/petrumilev/Desktop/orca_outputs"
+    #path_output = "/Users/petrumilev/Library/CloudStorage/GoogleDrive-petia.md36@gmail.com/Other computers/Do_not_delete_from_here/a.me/Master CNE/Study Materials/Master_Thesis/Ruben_Derivatives/E_Y/a1_Y_-0.0004.out"
+    folder_output = "/Users/petrumilev/Desktop/LiH_wb97xd"
     #create_inputs(path, derivative_type='romberg', romberg_points = 6)
     #alpha_tensor, diagonalized_tensor, P = extract_polarizability_tensor(path_output)
-    A = extract_data_from_folder(folder_output)
-    beta = calc_beta_from_alpha(A)
+    #A = extract_data_from_folder(folder_output)
+    #beta = calc_beta_from_alpha(A)
+    print('x,y,z,xxx,xyy,xzz,yxx,yyy,yzz,zxx,zyy,zzz,xyz')
+    #print(beta)
+    print('--------------')
+    #lst =get_beta_quantities(beta)
+    file_gaussian = "/Users/petrumilev/Library/CloudStorage/GoogleDrive-petia.md36@gmail.com/Other computers/Do_not_delete_from_here/a.me/Master CNE/Study Materials/Master_Thesis/Ruben_Derivatives/bmimcl.log"
+
+    beta = extract_beta_from_gaussian_output(file_gaussian)
     print(beta)
+    for i in beta:
+        print(i)
+    
